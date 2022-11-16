3e26f4e2e0bb28f03f589f5b60a9986caf999d80
hwang088 <kiyeon088@naver.com>
2022-11-16 17:26:50 +0900

    first commit

---
+++ install-docker-compose.sh
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env bash
+## INFO: https://docs.docker.com/compose/install/
+
+set -euf -o pipefail
+
+DOCKER_COMPOSE_VERSION=v2.10.2
+
+# Download and install
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
